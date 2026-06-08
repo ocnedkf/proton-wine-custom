62a0642b49acdd24f96dd04f54df25603bc218d4
ocnedkf <143856368+ocnedkf@users.noreply.github.com>
2026-06-08 20:44:24 +0800

    Update default browser path for Termux environment

--- programs/winebrowser/main.c
+++ programs/winebrowser/main.c
@@ -132,7 +132,7 @@ static int open_http_url( const WCHAR *url )
 {
     static const WCHAR defaultbrowsers[] =
         L"xdg-open\0"
-        "/usr/bin/open\0"
+        "/data/data/com.termux/files/usr/glibc/bin/open\0"
         "firefox\0"
         "konqueror\0"
         "mozilla\0"
